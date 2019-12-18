bd725581ee95a971d103b46da6ebd3462d67e974
Johannes Appel <58848423+JohannesAppel@users.noreply.github.com>
2019-12-18 16:46:55 +0200

    Add files via upload

---
+++ DataLogger/src/DataLogger/src/canbus.py
@@ -0,0 +1,71 @@
+# '''
+# Created on Dec 13, 2019
+# 
+# @author: Hex
+# '''
+import binascii
+from canlib import canlib, Frame
+from canlib.canlib import ChannelData 
+
+class CanBus:
+    def __init__(self, channel, cnt=4):
+        self.cnt = cnt
+        self.ch = canlib.openChannel(channel, canlib.canOPEN_ACCEPT_VIRTUAL)
+        print("Using channel: %s, EAN: %s" % (ChannelData(channel).channel_name,
+                                              ChannelData(channel).card_upc_no)
+                                                )
+        self.ch.setBusOutputControl(canlib.canDRIVER_NORMAL)
+        self.ch.setBusParams(canlib.canBITRATE_500K)
+        self.ch.busOn()
+    
+    def tearDownChannel(self):
+        self.ch.busOff()
+        self.ch.close()
+        
+    def counter(self):
+        try:
+            cnt = 0
+            frame = self.ch.read()
+            firstID = frame.id
+            while True:
+                frame = self.ch.read()
+                cnt += 1
+                if frame.id == firstID:
+                    break
+                    
+        except (canlib.canNoMsg):
+            pass
+        except (canlib.canError):
+            pass
+        return cnt
+    
+    def read_frame(self):
+        frame = []
+        for _ in range(self.cnt + 1):
+            frame.append(self.ch.read())
+        return frame
+
+def text(t):
+    tx = binascii.hexlify(t).decode('utf-8')
+    n = 2
+    txt = [tx[i:i+n] for i in range(0, len(tx), n)]
+    return txt  
+
+if __name__ == "__main__":
+    print("canlib version:", canlib.dllversion())
+    
+    # Read from the canbus    
+    cbus = CanBus(channel=0)
+    cnt = cbus.counter()
+    print("Counter: %d" %(cnt)) 
+    
+    i = 0
+    show = ""
+    while i <= cbus.cnt:
+        show = ""
+        frame = cbus.ch.read()
+        show = show + ("%s\t%s\n" %(frame.id, text(frame.data)))
+        print(show)
+        i += 1   
+        
+    cbus.tearDownChannel()
---
+++ DataLogger/src/DataLogger/src/main.py
@@ -0,0 +1,27 @@
+'''
+Created on Dec 13, 2019
+
+@author: Hex
+'''
+
+import tkinter as tk
+import canbus
+
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.protocol("WM_DELETE_WINDOW", self.quit)
+
+        # Define any widgets
+        self.text = tk.Text(self, height=6, width=60)
+        self.text.pack()
+
+    def quit(self):
+        # Add here any shutdown related statements
+        super().quit()
+        
+
+#self.ch0 = canbus(channel=0)
+
+if __name__ == "__main__":
+    App().mainloop()
