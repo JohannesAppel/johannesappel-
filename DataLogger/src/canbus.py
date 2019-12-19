17fedabf4276bac5a3ca81a51d4fe04886dc0e70
Johannes Appel <58848423+JohannesAppel@users.noreply.github.com>
2019-12-19 14:57:09 +0200

    Add files via upload

--- DataLogger/src/canbus.py
+++ DataLogger/src/canbus.py
@@ -6,17 +6,41 @@
 import binascii
 from canlib import canlib, Frame
 from canlib.canlib import ChannelData 
+import threading, queue
 
-class CanBus:
+class CanBus(threading.Thread):
     def __init__(self, channel, cnt=4):
+        super().__init__()
+        
         self.cnt = cnt
         self.ch = canlib.openChannel(channel, canlib.canOPEN_ACCEPT_VIRTUAL)
         print("Using channel: %s, EAN: %s" % (ChannelData(channel).channel_name,
-                                              ChannelData(channel).card_upc_no)
-                                                )
+                                              ChannelData(channel).card_upc_no))
         self.ch.setBusOutputControl(canlib.canDRIVER_NORMAL)
         self.ch.setBusParams(canlib.canBITRATE_500K)
         self.ch.busOn()
+        
+        self.queue = queue.Queue()
+        self._is_alive = threading.Event()
+        self._is_alive.set()
+        
+    def get(self):
+        # None blocking get
+        try:
+           data = self.queue.get(False)
+        except ???.QueueEmpty:
+           return None
+        return data
+        
+    def stop(self):
+        print('CanBus.stop()')
+        self._is_alive.clear()
+        while not self.queue.is_empty():
+            self.queue.get()
+    
+
+    def run(self):
+        # copy my sceleton
     
     def tearDownChannel(self):
         self.ch.busOff()
@@ -50,14 +74,7 @@ def text(t):
     txt = [tx[i:i+n] for i in range(0, len(tx), n)]
     return txt  
 
-if __name__ == "__main__":
-    print("canlib version:", canlib.dllversion())
-    
-    # Read from the canbus    
-    cbus = CanBus(channel=0)
-#     cbus.cnt = cbus.counter()
-#     print("Counter: %d" %(cbus.cnt)) 
-    
+def read4(cbus):
     j = 0
     show = ""
     while j < 4:
@@ -71,8 +88,25 @@ if __name__ == "__main__":
         if frame is not None:
             print("%s\t%s\n" %(frame.id, text(frame.data)))
         j += 1
-#             show = show + ("%s\t%s\n" %(frame.id, text(frame.data)))
-#             print(show)
+#        show = show + ("%s\t%s\n" %(frame.id, text(frame.data)))
+#        print(show)
+
+if __name__ == "__main__":
+    print("canlib version:", canlib.dllversion())
+    
+    # Read from the canbus    
+    cbus = CanBus(channel=0)
+#     cbus.cnt = cbus.counter()
+#     print("Counter: %d" %(cbus.cnt)) 
+
+    cbus.start()
+    # For testing we need to terminate the `Thread`
+    max_frames = 100
+    for _ in range(max_frames):
+        frame = cbus.get()
+        print(frame)
+    cbus.stop()
+    print('EXIT')
         
         
     cbus.tearDownChannel()
