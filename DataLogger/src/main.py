119d576365a6e3b90dcd42ea23ef744a96fd6a9f
Johannes Appel <58848423+JohannesAppel@users.noreply.github.com>
2019-12-20 14:01:23 +0200

    Add files via upload

--- DataLogger/src/main.py
+++ DataLogger/src/main.py
@@ -8,20 +8,32 @@ import tkinter as tk
 from canbus import CanBus
 
 class FrameListbox(tk.Listbox):
-    FRAMES_TO_SHOW = 4
-    
-    def __init__(self, parent, **kwargs):
-        super().__init__(parent, **kwargs)
+    def __init__(self, parent, lines=4, **kwargs):
+        super().__init__(parent, font=('Consolas', 10), **kwargs)
+
+        self._lines_to_show = lines
+        self._lines = 0
+        
+    def insert(self, frame):
+        if self._lines == self._lines_to_show:
+            self.delete(0, tk.END)
+            self._lines = 0
+        
+        super().insert(tk.END, frame)
+        self._lines += 1
+
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
         self.protocol("WM_DELETE_WINDOW", self.quit)
 
         # Define any widgets
-        self.text = tk.Text(self, height=6, width=60)
-        self.text.pack()
+        self.frame_listbox = FrameListbox(self, lines=self.cbus.cnt, height=6, width=60)
+        self.frame_listbox.pack()
         self.none_count = 0
         self.cbus = CanBus(channel=0)
         self.cbus.start()
         self.after(10, self.display)
-        self._count = 0
 
     def quit(self):
         # Add here any shutdown related statements
@@ -34,20 +46,13 @@ class FrameListbox(tk.Listbox):
         if self.cbus.is_alive():
             frame = self.cbus.get()
             if frame is not None:
-                self.frame_listbox.insert("%s\t%s" %(frame.id, frame.data))
+                self.frame_listbox.insert(tk.END, "%s\t%s\n" %(frame.id, frame.data))
             else:
                 self.none_count += 1
 
             self.after(10, self.display)    
-    
-    def insert(self, frame):
-        if self._count == FrameListbox.FRAMES_TO_SHOW:
-            self.delete(0, tk.END)
-            self._count = 0
-        
-        super().insert(tk.END, frame)
-        self._count += 1
+
 #self.ch0 = canbus(channel=0)
 
 if __name__ == "__main__":
-    FrameListbox().mainloop()
+    App().mainloop()
