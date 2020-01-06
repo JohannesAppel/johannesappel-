de2f65e43484ec5b96672a78a203433206b6236e
Johannes Appel <58848423+JohannesAppel@users.noreply.github.com>
2020-01-06 11:04:40 +0200

    Add files via upload

--- LogData/src/main.py
+++ LogData/src/main.py
@@ -38,41 +38,68 @@ def text(t):
     return txt
 
 def counter():
-    try:
-        cnt = 0
-        frame = ch0.read()
-        firstID = frame.id
-        while True:
+    listCnt = []
+    j = 0
+    firstID = 0
+    while j < 1000:
+        try:
             frame = ch0.read()
-            cnt += 1
-            if frame.id == firstID:
-                break 
-    except (canlib.canNoMsg):
-        pass
-    except (canlib.canError):
-        pass
-    return cnt
+            firstID = frame.id
+            while True:
+                frame = ch0.read()
+                if frame.id not in listCnt:
+                    listCnt.append(frame.id)
+                if frame.id == firstID:
+                    break  
+        except (canlib.canNoMsg):
+            pass
+        j += 1
+        time.sleep(0.01)        
+#     try:
+#         cnt = 0
+#         frame = ch0.read()
+#         firstID = frame.id
+#         while True:
+#             frame = ch0.read()
+#             cnt += 1
+#             if frame.id == firstID:
+#                 break 
+#     except (canlib.canNoMsg):
+#         pass
+#     except (canlib.canError):
+#         pass
+    listCnt.sort()
+    print(listCnt)
+    return len(listCnt)
 
 def display():
     T.delete("1.0", "end")
+    dispList = [[],[]]
     show = ""
     i = 1
     while i <= cnt:
         try:
             frame = ch0.read()
+            if frame.id not in dispList:
+                dispList.extend([frame.id, frame.data])
+            else:
+                inx = dispList.index(frame.id)
+                dispList[inx] = [frame.id], [frame.data]
+                #dispList()
+            #print(dispList)
             show = show + ("%s\t%s\n" %(frame.id, text(frame.data)))
             i += 1             
         except (canlib.canNoMsg):
             pass
-    print(show)            
+    print(show)
     T.insert("end", show)
     root.after(1, display)
 
-def cycle(cnt):
+def cycle(firstID):
     while True:
         try:
             frame = ch0.read()
-            if frame.id == cnt:
+            if frame.id == firstID:
                 break
         except (canlib.canNoMsg):
             pass
@@ -84,11 +111,11 @@ ch0 = setUpChannel(channel=0)
 cnt = counter()
 print("Counter: %d" %(cnt))
 
-T = tk.Text(root, height=6, width=60)
+T = tk.Text(root, height=20, width=100)
 T.config(state="normal")
 T.pack()
 
-cycle(cnt)
+#cycle()
 
 display() 
 root.mainloop()     
