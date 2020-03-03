a93b4368c93a4f7cc8671f2c14c6f6dc9a995803
Johannes Appel <58848423+JohannesAppel@users.noreply.github.com>
2020-03-03 16:44:21 +0200

    Add files via upload

---
+++ Core/Inc/global_variables.h
@@ -0,0 +1,13 @@
+/*
+ * global_variables.h
+ *
+ *  Created on: Mar 2, 2020
+ *      Author: Johannes
+ */
+
+#ifndef INC_GLOBAL_VARIABLES_H_
+#define INC_GLOBAL_VARIABLES_H_
+
+
+
+#endif /* INC_GLOBAL_VARIABLES_H_ */
---
+++ Core/Src/global_variables.c
@@ -0,0 +1,8 @@
+/*
+ * global_variables.c
+ *
+ *  Created on: Mar 2, 2020
+ *      Author: Johannes
+ */
+
+
